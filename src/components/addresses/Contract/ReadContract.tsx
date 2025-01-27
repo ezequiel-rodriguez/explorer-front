1cfc9d5b8ee2d127c8c79f886fd451651998e46b
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2025-01-27 12:17:33 -0300

    wip: add contract interactions method accordion. Iterate read methods

--- src/components/addresses/Contract/ReadContract.tsx
+++ src/components/addresses/Contract/ReadContract.tsx
@@ -1,12 +1,68 @@
-// import { useAddressDataContext } from '@/context/AddressContext'
+import ListContent from '@/components/generals/ListContent'
+import ListItem from '@/components/generals/ListItem'
+import CIMAccordion from '@/components/ui/CIMAccordion'
+import { useAddressDataContext } from '@/context/AddressContext'
+import { ethers } from 'ethers'
 import React from 'react'
 
 function ReadContract() {
-  // const { address, contractVerification } = useAddressDataContext()
-  // console.dir({ address, contractVerification }, { depth: null })
+  const { address, contractVerification } = useAddressDataContext()
+
+  if (!contractVerification) {
+    return <div>Loading...</div>
+  }
+
+  const { abi } = contractVerification
+
+  const constructor = abi.find(fragment => fragment.type === 'constructor')
+  const fallback = abi.find(fragment => fragment.type === 'fallback')
+  const receive = abi.find(fragment => fragment.type === 'receive')
+  const error = abi.find(fragment => fragment.type === 'error')
+  const events = abi.filter(fragment => fragment.type === 'event')
+  const methods = abi.filter(fragment => fragment.type === 'function')
+  const readMethods = methods.filter(fragment => fragment.stateMutability === 'view' || fragment.stateMutability === 'pure')
+  const writeMethods = methods.filter(fragment => fragment.stateMutability === 'payable' || fragment.stateMutability === 'nonpayable')
+
+  console.dir({ constructor, fallback, receive, error, events, methods, readMethods, writeMethods })
+  
   return (
     <div>
-      Read Contract Methods
+      <div className='flex flex-col gap-2'>
+        {readMethods.map((method, i) => {
+          const index = i + 1
+          const methodTitleProps = {
+            index,
+            methodName: method.name as string,
+            selectorHash: "0x1234ABcD"// ethers.id(method) // TODO: check how to get the selector hash
+          }
+
+          return (
+            // TODO: refactor Accordion style or create a different one
+            <CIMAccordion key={index} title={<MethodTitle {...methodTitleProps} />} className='bg-secondary'>
+              <div className='p-4'>
+                <p>{method.name}</p>
+              </div>
+            </CIMAccordion>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+const MethodTitle = ({
+  index,
+  methodName,
+  selectorHash
+}: {
+  index: number
+  methodName: string
+  selectorHash: string
+}): React.ReactNode => {
+  return (
+    <div className='flex gap-1 items-center'>
+      <span className='text-sm'>{index}. {methodName}</span>
+      <span className='text-sm text-[#b9b9b9]'>{`(${selectorHash})`}</span>
     </div>
   )
 }
---
+++ src/components/ui/CIMAccordion.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { useState, useRef } from "react";
+
+type Props = {
+  title: string | React.ReactNode;
+  className?: string;
+  children: React.ReactNode;
+};
+
+const CIMAccordion = ({ title, className, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`w-full ${className}`}>
+      {/* Button to toggle accordion */}
+      <button
+        onClick={toggleAccordion}
+        className={`w-full flex justify-between items-center bg-[#262626] text-white px-4 py-2 transition-all duration-300 ${isOpen ? 'rounded-t-xl' : 'rounded-xl'}`}
+      >
+        {title}
+        <span className="text-md">{isOpen ? "< >" : "<>"}</span>
+      </button>
+
+      {/* Accordion Content Wrapper */}
+      <div
+        className={`grid transition-[grid-template-rows] duration-300 ease-in-out ${
+          isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"
+        }`}
+      >
+        {/* Accordion Content */}
+        <div
+          ref={contentRef}
+          className="overflow-hidden bg-[#141414] rounded-b-2xl text-gray-300 text-sm"
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CIMAccordion;
