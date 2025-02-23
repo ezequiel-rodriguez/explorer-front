fb163ed4090262bbca49025f92b1753de9ac813c
Carlos Rosado <carlos.rosado@rootstocklabs.com>
2025-02-23 15:47:24 -0600

    style: formatting

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,8 +6,12 @@ const error = () => {
   return (
     <div className="w-full h-screen flex justify-center mt-8">
       <div className="w-full max-h-fit flex flex-col text-center bg-secondary p-6 sm:p-12 rounded-xl">
-        <div className='text-4xl sm:text-[80px] font-bold mb-4 sm:mb-16'>Ooops</div>
-        <div className="text-lg sm:text-[38px] mb-6 sm:mb-10 font-bold">An error has occurred</div>
+        <div className="text-4xl sm:text-[80px] font-bold mb-4 sm:mb-16">
+          Ooops
+        </div>
+        <div className="text-lg sm:text-[38px] mb-6 sm:mb-10 font-bold">
+          An error has occurred
+        </div>
         <Link
           className="btn px-4 w-fit mx-auto py-2 bg-brand-orange rounded-xl text-black font-medium"
           href={ROUTER.HOME}
