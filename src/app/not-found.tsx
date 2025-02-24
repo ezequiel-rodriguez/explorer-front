902c93e6049efe36dc4573b501b7786af4a601ba
krlz16 <149804324+krlz16@users.noreply.github.com>
2025-02-24 04:15:25 -0600

    Feat: enhance error handling and add custom 404 page (#28)
    * Feat: enhance error handling and add custom 404 page
    
    * style: formatting
    
    ---------
    
    Co-authored-by: Carlos Rosado <carlos.rosado@rootstocklabs.com>

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,19 @@
-'use client';
 import { ROUTER } from '@/common/constants';
 import Link from 'next/link';
 
-const error = () => {
+export default function NotFound() {
   return (
     <div className="w-full h-screen flex justify-center mt-8">
       <div className="w-full max-h-fit flex flex-col text-center bg-secondary p-6 sm:p-12 rounded-xl">
         <div className="text-4xl sm:text-[80px] font-bold mb-4 sm:mb-16">
-          Ooops
+          404
         </div>
         <div className="text-lg sm:text-[38px] mb-6 sm:mb-10 font-bold">
-          An error has occurred
+          Page Not Found.
+        </div>
+        <div className="text-white-400 mb-6">
+          We couldn't find this page! Use the search bar to find what you're
+          looking for or go back to the home page
         </div>
         <Link
           className="btn px-4 w-fit mx-auto py-2 bg-brand-orange rounded-xl text-black font-medium"
@@ -21,6 +24,4 @@ const error = () => {
       </div>
     </div>
   );
-};
-
-export default error;
+}
